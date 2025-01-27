1e39ee333b90bf1e09b37f0b700ef20f05db6c6c
Jad-Bel <jadthegamer06@gmail.com>
2025-01-27 16:50:18 +0100

    Add User model and Database connection class for authentication

--- App/models/User.php
+++ App/models/User.php
@@ -1,1 +1,12 @@
 <?php
+
+require_once __DIR__ . '/../../config/Database.php';
+
+class User {
+    private $db;
+
+    public function __construct() 
+    {
+        $this->db = new Database();
+    }
+}
--- config/Database.php
+++ config/Database.php
@@ -1,1 +1,21 @@
 <?php
+
+class Database {
+    private $host = 'localhost';
+    private $un = 'your_ussername';
+    private $pw = 'your_password';
+    private $db = 'your_database';
+    private $conn;
+
+    public function getConnection()
+    {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db}", $this->un, $this->pw);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            throw new Exception("Connection error: " . $e->getMessage());
+        }
+        return $this->conn;
+    }
+}

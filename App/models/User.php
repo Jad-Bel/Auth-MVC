745e52b22c93ac5d217bfe01dcb0118c58d092ee
Jad-Bel <jadthegamer06@gmail.com>
2025-01-27 16:54:06 +0100

    Refactor User model to use connection from Database class and add user registration method

--- App/models/User.php
+++ App/models/User.php
@@ -3,10 +3,25 @@
 require_once __DIR__ . '/../../config/Database.php';
 
 class User {
-    private $db;
+    private $conn;
 
     public function __construct() 
     {
-        $this->db = new Database();
+        $db = new Database();
+        $this->conn = $db->getConnection();
     }
+
+    public function register ($username, $email, $password) 
+    {
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        $query = 'INSERT INTO users (username, email, password) VALUES (:username, :email, :password)';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":username", $username);
+        $stmt->bindParam(":email", $email);
+        $stmt->bindParam(":password", $password);
+
+        return $stmt->execute();
+    }
+
+    
 }

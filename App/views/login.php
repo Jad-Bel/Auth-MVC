2fb5d06afe3fedbb98f07f79997cd4533756923c
Jad-Bel <jadthegamer06@gmail.com>
2025-01-27 18:13:14 +0100

    Add error and success message display in login view

--- App/views/login.php
+++ App/views/login.php
@@ -1,14 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="../../public/css/style.css">
 </head>
+
 <body>
     <div class="container">
         <h2>Login</h2>
+        <?php
+        if (isset($_SESSION['error'])) {
+            echo '<p class="error">' . $_SESSION['error'] . '</p>';
+            unset($_SESSION['error']);
+        }
+        if (isset($_SESSION['success'])) {
+            echo '<p class="success">' . $_SESSION['success'] . '</p>';
+            unset($_SESSION['success']);
+        }
+        ?>
         <form id="loginForm" action="../../index.php" method="post">
             <input type="hidden" name="action" value="login">
             <div class="form-group">
@@ -24,4 +36,5 @@
         <p>Don't have an account? <a href="register.php">Register here</a></p>
     </div>
 </body>
+
 </html>

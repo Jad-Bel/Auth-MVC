90df7ffe6043ad9c435ada708beeb87942d329ff
Jad-Bel <jadthegamer06@gmail.com>
2025-01-27 16:44:51 +0100

    Implement AuthController with request handling for user registration and login

--- App/controller/AuthController.php
+++ App/controller/AuthController.php
@@ -1,1 +1,30 @@
 <?php
+
+use Couchbase\User;
+
+require_once  __DIR__ . '/../models/User.php';
+
+class AuthController {
+    private $user;
+
+    public function __construct() {
+        $this->user = new User();
+    }
+
+    public function handleRequest () {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $action = $_POST["action"] ? '';
+
+            switch ($action) {
+                case 'register':
+                    $this->handleRegister();
+                    break;
+                case 'login':
+                    $this->handleLogin();
+                    break;
+                default:
+                    echo 'Error: Invalid Action';
+            }
+        }
+    }
+}

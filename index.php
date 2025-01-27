45959e21057fb75e526bfd0daf4808bf096e0480
Jad-Bel <jadthegamer06@gmail.com>
2025-01-27 17:33:51 +0100

    Update database configuration and include login view in index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 require_once __DIR__ . '/App/controller/AuthController.php';
+require_once __DIR__ . '/App/views/login.php';
+
 $authController = new AuthController();
 $authController->handleRequest();
+
 

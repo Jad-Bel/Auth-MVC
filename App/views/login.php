9affbb588a19bccbc558f977a235647bb529aa0b
Jad-Bel <jadthegamer06@gmail.com>
2025-01-30 15:13:06 +0100

    Fix stylesheet path in login and register views for correct loading

--- App/views/login.php
+++ App/views/login.php
@@ -9,7 +9,7 @@ print_r($_SESSION);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="/public/css/style.css">
+    <link rel="stylesheet" href="public/css/style.css">
 </head>
 
 <body>
